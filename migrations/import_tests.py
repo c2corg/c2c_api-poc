e0ce2931465e1dd951e9a4fd863f7e30335bb0a9
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-10-13 13:56:15 +0200

    Separate legacy test from validated tests

--- migrations/import_tests.py
+++ migrations/import_tests.py
@@ -6,7 +6,7 @@ import subprocess
 subprocess.run(["rm", "-rf", "tests"], check=True)
 
 for sub_dir, folders, files in os.walk("../v6_api/c2corg_api/tests"):
-    dest_dir = sub_dir.replace("../v6_api/c2corg_api/", "")
+    dest_dir = sub_dir.replace("../v6_api/c2corg_api/tests", "tests/legacy")
     Path(dest_dir).mkdir(parents=True, exist_ok=True)
 
     for file in files:
@@ -15,7 +15,8 @@ for sub_dir, folders, files in os.walk("../v6_api/c2corg_api/tests"):
             dest = os.path.join(dest_dir, file)
             shutil.copyfile(source, dest)
 
-subprocess.run(["mv", "tests/__init__.py", "tests/__init__LEGACY.py"], check=True)
-subprocess.run(["rm", "tests/markdown/__init__.py"], check=True)
+subprocess.run(["rm", "tests/legacy/markdown/__init__.py"], check=True)
+
+subprocess.run(["mv", "tests/legacy/markdown", "tests/markdown"], check=True)
 
 subprocess.run(["black", "."], check=True)
