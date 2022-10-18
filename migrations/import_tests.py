d9321de8a9d49244b0ef3d4225254c0b8c11712f
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-10-18 18:43:20 +0200

    update flask-camp version, project settings

--- migrations/import_tests.py
+++ migrations/import_tests.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path
 import re
-import shutil
+import black
 import subprocess
+
 
 replacements = [
     (r"# package\n*", ""),
@@ -69,63 +70,44 @@ replacements = [
 ]
 
 
-def reimport_all():
-    subprocess.run(["rm", "-rf", "tests/legacy"], check=True)
-
-    for sub_dir, _, files in os.walk("../v6_api/c2corg_api/tests"):
-        dest_dir = sub_dir.replace("../v6_api/c2corg_api/tests", "c2corg_api/tests/legacy")
-        Path(dest_dir).mkdir(parents=True, exist_ok=True)
-
-        for file in files:
-            if not file.endswith(".pyc"):
-                source = os.path.join(sub_dir, file)
-                dest = os.path.join(dest_dir, file)
-                shutil.copyfile(source, dest)
-
-    subprocess.run(["black", "c2corg_api/tests"], check=True)
-
-
 def convert_test_file(filename):
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(f"../v6_api/c2corg_api/tests/{filename}", "r", encoding="utf-8") as f:
         code = "".join(f.readlines())
+
+    code = black.format_str(code, mode=black.Mode(line_length=120))
 
     for pattern, new_value in replacements:
         code = re.sub(pattern, new_value, code)
+
+    code = black.format_str(code, mode=black.Mode(line_length=120))
 
     # remove empty init file
     if filename.endswith("__init__.py"):
         if len(code) == 0:
             return
 
-    filename = filename.replace("legacy/", "")
-
-    Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
-
-    with open(filename, "w", encoding="utf-8") as f:
+    dest = f"c2corg_api/tests/{filename}"
+    Path(os.path.dirname(dest)).mkdir(parents=True, exist_ok=True)
+    with open(dest, "w", encoding="utf-8") as f:
         f.write(code)
 
 
 def convert_test_folder(folder):
-    subprocess.run(["rm", "-rf", folder.replace("legacy/", "")], check=True)
+    subprocess.run(["rm", "-rf", f"c2corg_api/tests/{folder}"], check=True)
 
-    for sub_dir, _, files in os.walk(folder):
+    for sub_dir, _, files in os.walk(f"../v6_api/c2corg_api/tests/{folder}"):
         for file in files:
             filename = os.path.join(sub_dir, file)
-            dest = filename.replace("legacy/", "")
 
             if filename.endswith(".py"):
-                convert_test_file(filename)
+                convert_test_file(filename.replace("../v6_api/c2corg_api/tests/", ""))
             else:
-
+                dest = filename.replace("../v6_api/", "")
                 Path(os.path.dirname(dest)).mkdir(parents=True, exist_ok=True)
                 subprocess.run(["cp", filename, dest], check=True)
 
 
-# # reimport_all()
-# convert_test_folder("c2corg_api/tests/legacy/markdown")
-# convert_test_file("c2corg_api/tests/legacy/views/test_health.py")
-# convert_test_file("c2corg_api/tests/legacy/views/test_cooker.py")
-
-convert_test_file("c2corg_api/tests/legacy/views/test_user.py")
-
-subprocess.run(["black", "c2corg_api/tests"], check=True)
+convert_test_folder("markdown")
+convert_test_file("views/test_health.py")
+convert_test_file("views/test_cooker.py")
+convert_test_file("views/test_user.py")
