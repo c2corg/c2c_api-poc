353a51cb8155502dcd777b5c2c7c4db4cc435c9b
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-10-21 23:06:18 +0200

    est_get_preferences_lang

--- c2corg_api/legacy/views/users/preferences.py
+++ c2corg_api/legacy/views/users/preferences.py
@@ -1,6 +1,4 @@
-from copy import deepcopy
 from flask import request
-from flask_login import current_user
 from flask_camp import allow, current_api
 from flask_camp.views.account import current_user as current_user_view
 
@@ -9,8 +7,13 @@ rule = "/users/preferences"
 
 @allow("authenticated", allow_blocked=True)
 def get():
+    lang = request.args.get("pl", None)
     result = current_user_view.get()["user"]["ui_preferences"]["feed"]
 
     result["areas"] = [current_api.get_cooked_document(area_id)["legacy"] for area_id in result["areas"]]
+
+    if lang is not None:
+        for area in result["areas"]:
+            area["locales"] = [locale for locale in area["locales"] if locale["lang"] == lang]
 
     return result
