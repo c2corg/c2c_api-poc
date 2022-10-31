7b785c33ea884ef732a33435574292c172ccc58c
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2022-10-31 12:25:18 +0100

    remove useless check

--- c2corg_api/hooks/_on_user_update.py
+++ c2corg_api/hooks/_on_user_update.py
@@ -1,8 +1,7 @@
 from flask import request, current_app
 from flask_camp import current_api
 from flask_camp.models import User
-from sqlalchemy.exc import IntegrityError
-from werkzeug.exceptions import BadRequest, NotFound
+from werkzeug.exceptions import NotFound
 
 from c2corg_api.hooks._tools import check_user_name
 from c2corg_api.security.discourse_client import get_discourse_client
@@ -10,17 +9,10 @@ from c2corg_api.security.discourse_client import get_discourse_client
 
 def on_user_update(user: User, sync_sso=True):
 
-    # as now, flask_camp does not allow to modify user name. To be discussed (maybe a modo action)
-    # v6 API allows it, so we do that here
     data = request.get_json()
 
     if "name" in data:
-        user.name = data["name"]
         check_user_name(user.name)
-        try:
-            current_api.database.session.flush()
-        except IntegrityError as e:
-            raise BadRequest("Name or email already exists") from e
 
     follow = list(set(user.data["feed"]["follow"]))
     user.data["feed"]["follow"] = follow  # remove duplicates
